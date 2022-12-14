fad203437a96b22ac940ea929e9dee90d53bcff5
altrntn-code <118061169+altrntn-code@users.noreply.github.com>
2022-12-14 17:02:38 +0100

    Update IndexPage.tsx

--- src/containers/IndexPage/IndexPage.tsx
+++ src/containers/IndexPage/IndexPage.tsx
@@ -16,41 +16,6 @@ export default function IndexPage() {
               <S.ProjectLink>1. First Person Controls</S.ProjectLink>
             </Link>
           </S.ProjectContainer>
-          <S.ProjectContainer>
-            <Link href="/projects/image-transitions" passHref>
-              <S.ProjectLink>2. GLSL Image Transitions</S.ProjectLink>
-            </Link>
-          </S.ProjectContainer>
-          <S.ProjectContainer>
-            <Link href="https://react-just-parallax.michalzalobny.com/" passHref>
-              <S.ProjectLink target="_blank">3. React Just Parallax</S.ProjectLink>
-            </Link>
-          </S.ProjectContainer>
-          <S.ProjectContainer>
-            <Link href="/projects/meta-office" passHref>
-              <S.ProjectLink>4. Meta Office</S.ProjectLink>
-            </Link>
-          </S.ProjectContainer>
-          <S.ProjectContainer>
-            <Link href="/projects/exploding-particles" passHref>
-              <S.ProjectLink>5. Exploding Particles</S.ProjectLink>
-            </Link>
-          </S.ProjectContainer>
-          <S.ProjectContainer>
-            <Link href="/projects/drop-unveil" passHref>
-              <S.ProjectLink>6. Drop Unveil</S.ProjectLink>
-            </Link>
-          </S.ProjectContainer>
-          <S.ProjectContainer>
-            <Link href="/projects/shader-blob" passHref>
-              <S.ProjectLink>7. Shader Blob</S.ProjectLink>
-            </Link>
-          </S.ProjectContainer>
-          <S.ProjectContainer>
-            <S.ProjectLink target="_blank" href="https://michalzalobny.com/">
-              8. Portfolio 2021
-            </S.ProjectLink>
-          </S.ProjectContainer>
         </S.ProjectsWrapper>
       </S.Wrapper>
     </>
