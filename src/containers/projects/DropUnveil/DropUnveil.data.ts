95c2df997cc5e8d56e8f2ba033729d5606d67ddf
altrntn-code <118061169+altrntn-code@users.noreply.github.com>
2022-12-16 01:16:08 +0100

    Update DropUnveil.data.ts

--- src/containers/projects/DropUnveil/DropUnveil.data.ts
+++ src/containers/projects/DropUnveil/DropUnveil.data.ts
@@ -5,9 +5,9 @@ import { PageProps } from 'utils/sharedTypes';
 
 export const getStaticProps: GetStaticProps = () => {
   const head: PageProps['head'] = {
-    ogImage: 'https://res.cloudinary.com/dpv0ukspz/image/upload/v1650804189/ogdrop_yvrwue.jpg',
-    title: 'Drop unveil',
-    description: 'Michal Zalobny portfolio 2022 WebGL & GLSL',
+    ogImage: 'https://none.jpg',
+    title: 'Shader BG',
+    description: 'Shader',
   };
 
   return {
@@ -15,8 +15,7 @@ export const getStaticProps: GetStaticProps = () => {
       head,
       inspirationName: 'Monopo London',
       inspirationHref: 'https://monopo.london/',
-      repoHref:
-        'https://github.com/michalzalobny/creative-bay/tree/main/src/containers/projects/DropUnveil',
+      repoHref: 'https://github.com/none/shader/tree/main/src/containers/projects/shader',
     },
     revalidate: sharedValues.ISR_TIMEOUT,
   };
